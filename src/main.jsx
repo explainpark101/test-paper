7cbf3a01d7554769f3e128fc9cdfd4ff9b010f64
Jaehyung Park <explainpark101@gmail.com>
2026-03-18 21:02:58 +0900

    feat: Implement RootLayout with ScrollToTop functionality and restructure routing for improved navigation

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,41 @@
+import { useEffect } from "react";
 import AppGemini from "./AppGemini";
 import { GeminiKeyProvider } from "./contexts/GeminiKeyContext";
 import PrintPreviewPage from "./pages/PrintPreviewPage";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, Outlet, RouterProvider, useLocation } from "react-router";
 import "./index.css";
 
 const basePath = import.meta.env.VITE_BASE_PATH || '/';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
+function RootLayout() {
+  return (
+    <>
+      <ScrollToTop />
+      <Outlet />
+    </>
+  );
+}
+
 const router = createBrowserRouter([
-  { path: "/", element: <GeminiKeyProvider><AppGemini /></GeminiKeyProvider> },
-  // 구주소(/paper/:id, /paper/:id/:view) 호환용 라우트
-  { path: "/paper/:id", element: <GeminiKeyProvider><AppGemini /></GeminiKeyProvider> },
-  { path: "/paper/:id/:view", element: <GeminiKeyProvider><AppGemini /></GeminiKeyProvider> },
-  { path: "/print-preview", element: <PrintPreviewPage /> },
-  { path: "/print-preview/memo", element: <PrintPreviewPage /> },
+  {
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <GeminiKeyProvider><AppGemini /></GeminiKeyProvider> },
+      { path: "paper/:id", element: <GeminiKeyProvider><AppGemini /></GeminiKeyProvider> },
+      { path: "paper/:id/:view", element: <GeminiKeyProvider><AppGemini /></GeminiKeyProvider> },
+      { path: "print-preview", element: <PrintPreviewPage /> },
+      { path: "print-preview/memo", element: <PrintPreviewPage /> },
+    ],
+  },
 ], {
   basename: basePath === '/' ? undefined : basePath.replace(/\/$/, ''),
 });
